71764cb80f930f425e6a2df61e07a5fa6665f895
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-05-02 18:39:12 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+float massimo(float val [] , int a)
+{
+    float max=0;
+    max=val[0];
+    for(int i=1;i<a;i++)
+    {
+        if(val[i]>max)
+        {
+            max=val[i];
+        }
+    }
+    return max;
+}
