211bf8d73ccb91a878a3789e44889e1efb7bdf43
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-05-02 18:41:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+int n;
+float valori[100];
+cin>>n;
+for(int i=0;i<n;i++)
+{
+ cin>>valori[i];
+}
+cout<<massimo(valori,n);
+    return 0;
 }
